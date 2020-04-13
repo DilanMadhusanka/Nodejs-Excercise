d7c18c2b2f765a2dc92973e64565e0e9d6b61eb4
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-04-13 10:05:58 +0530

    responding with html

--- 01. First Server/index.js
+++ 01. First Server/index.js
@@ -1,14 +1,20 @@
 const http = require('http')
 
+const fs = require('fs')
+
+const homePage = fs.readFileSync('index.html')
+const aboutPage = fs.readFileSync('about.html')
+const contactPage = fs.readFileSync('contact.html')
+
 const server = http.createServer((request, response) => {
     console.log(request.url)
 
     if (request.url === '/about') {
-        return response.end('THE ABOUT PAGE')
+        return response.end(aboutPage)
     } else if (request.url === '/contact') {
-        return response.end('THE CONTACT PAGE')
+        return response.end(contactPage)
     } else if (request.url === '/') {
-        return response.end('THE HOME PAGE')
+        return response.end(homePage)
     } else {
         response.writeHead(404)
         return response.end('THE PAGE WAS NOT FOUND')
