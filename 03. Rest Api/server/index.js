f52df21ea5649ad264575f03172342f5ca8f54ca
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-04-14 20:13:43 +0530

    do post request in register.js

--- 03. Rest Api/server/index.js
+++ 03. Rest Api/server/index.js
@@ -4,6 +4,12 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
+const register = require('./routes/Auth/register')
+
+app.use(express.json())
+
+app.use(register)
+
 app.get('/test', (req, res) =>{
     return res.json("Hello world")
 })
