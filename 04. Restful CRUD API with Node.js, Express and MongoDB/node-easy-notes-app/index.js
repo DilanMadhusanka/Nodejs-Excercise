b198e8b971e334fe314acf7509507d37f8534441
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-04-15 09:07:47 +0530

    import database configuration and connect to the database using mongoos

--- 04. Restful CRUD API with Node.js, Express and MongoDB/node-easy-notes-app/index.js
+++ 04. Restful CRUD API with Node.js, Express and MongoDB/node-easy-notes-app/index.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const dbConfig = require('./config/database.config.js')
+const mongoose = require('mongoose')
+
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
+mongoose.Promise = global.Promise
+
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database")
+}).catch(err => {
+    console.log('Could not connect to the database. Existing now...', err)
+    process.exit()
+})
 
 app.get('/', (req, res) => {
     res.json({ "message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes." })
